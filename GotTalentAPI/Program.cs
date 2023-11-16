ddfea190e406d904badc62cef3204d563d2c8df3
hoanghaiduong <hoanghaiduong1711@gmail.com>
2023-11-16 07:25:57 +0700

    commit

--- GotTalentAPI/Program.cs
+++ GotTalentAPI/Program.cs
@@ -7,12 +7,21 @@ namespace GotTalentAPI
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAllOrigins",
+                    builder =>
+                    {
+                        builder.AllowAnyOrigin()
+                               .AllowAnyMethod()
+                               .AllowAnyHeader();
+                    });
+            });
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-
+            
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
@@ -21,7 +30,7 @@ namespace GotTalentAPI
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
-            
+            app.UseCors("AllowAllOrigins");
             app.UseHttpsRedirection();
             app.UseAuthentication();
 
